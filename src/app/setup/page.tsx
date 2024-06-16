1870ff8149ce6d85114b2b04567b0ec7b643f214
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-06-16 18:22:28 +1000

    Created setup page and accompanying viewmodel

---
+++ src/app/setup/page.tsx
@@ -0,0 +1,37 @@
+"use client";
+import SetupPageViewModel from '@/viewmodels/SetupPageViewModel';
+import SimpleSelect from '../components/SimpleSelect';
+import SubmitButton from '../components/SubmitButton';
+
+/**
+ * Daemon setup screen.
+ * 
+ * Allows the user to modify the daemon's configuration via the GUI
+ * instead of manually editing the config.json file.
+ * 
+ * @returns Setup screen
+ */
+export default function SetupPage(){
+
+    const model = SetupPageViewModel()
+
+    return (
+        <form>
+            <div className="mb-3">
+                <label htmlFor="driver" className="form-label">Connection Type</label>
+                <SimpleSelect id="driver" values={model.drivers} setValue={model.setDriver}/>
+            </div>
+            <div className="mb-3">
+                <label htmlFor="panel" className="form-label">Panel</label>
+                <SimpleSelect id="panel" values={model.panels} setValue={model.setPanel}/>
+            </div>
+            <SubmitButton
+                classes="btn btn-success"
+                text="Save"
+                enabled={!model.saving}
+                onClick={model.save}
+                />
+        </form>
+    )
+}
+
---
+++ src/viewmodels/SetupPageViewModel.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react"
+
+/**
+ * Viewmodel for the setup screen.
+ * 
+ * Responsible for providing configuration options to the screen
+ * for the user to select, and enables facilitates the saving
+ * of those configuration options.
+ * 
+ * @returns Setup screen's viewmodel
+ */
+export default function SetupPageViewModel() : ISetupPageViewModel{
+
+    const usbDriver = "USB"
+    const gpioDriver = "GPIO"
+    const drivers = [usbDriver, gpioDriver]
+    const IT8951Panel = "IT8951"
+    const usbPanels = [IT8951Panel]
+    const gpioPanels = ['Not yet supported']
+
+    const [driver, setDriver] = useState(usbDriver)
+    const [panel, setPanel] = useState(IT8951Panel)
+    const [panels, setPanels] = useState<Array<string>>([])
+    const [saving, setSaving] = useState<boolean>(false)
+
+    // Whenever the panels variable changes, reset the panel
+    // variable to whatever the first option is.
+    useEffect(() => {
+        setPanel(panels[0])
+    }, [panels])
+
+    // When the user selects a driver, change the available
+    // panels to match the driver selection.
+    useEffect(() => {
+        if (driver === usbDriver)
+            setPanels(usbPanels)
+        else
+            setPanels(gpioPanels)
+    }, [driver])
+
+    // Function for saving the user's configs to disk
+    const save = () => {
+        setSaving(true)
+        //TODO perform ajax request
+        setSaving(false)
+    }
+
+    return {
+        drivers,
+        panels,
+        driver, setDriver,
+        setPanel,
+        saving, save
+    }
+
+}
+
+interface ISetupPageViewModel{
+    drivers: Array<string>;
+    panels: Array<string>;
+    driver: string;
+    setDriver: (value: string) => void;
+    setPanel: (value: string) => void;
+    saving: boolean;
+    save: () => void;
+}
