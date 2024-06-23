4eaea0cc6c5a05fe5f845d6e31d96861f18bd62a
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-06-23 19:53:07 +1000

    Implemented function for USB driver class initialization

--- src/drivers/usb/AbstractUsbDriver.tsx
+++ src/drivers/usb/AbstractUsbDriver.tsx
@@ -5,12 +5,83 @@ import IUsbDriver from "@/interfaces/IUsbDriver";
 import { BitsPerPixel } from "@/enums/BitsPerPixel";
 import Image from "@/classes/Image";
 import AbstractDriver from "../AbstractDriver";
+import { usb, findByIds, InEndpoint, OutEndpoint, Endpoint, LibUSBException } from 'usb';
 
-export default abstract class AbstractUsbDriver extends AbstractDriver implements IUsbDriver{
+/**
+ * Connects asynchronously to a given USB device.
+ * 
+ * The vendor and product IDs are what identify a specific
+ * USB device.
+ * You can find these by using the `lsusb` program.
+ * Expected output of that program is something like:
+ * Bus 001 Device 001: ID 1d6b:0002 Linux Foundation 2.0 root hub
+ * Where `1d6b` is the vendor ID and `0002` is the product ID.
+ * 
+ * @param vendorId Number representing vendor identifier
+ * @param productId Number representing product identifier
+ * @param device_interface_number Which USB interface to access
+ * @param endpoint_in_number Which endpoint of the USB interface provides input
+ * @param endpoint_out_number Which endpoint of the USB interface provides output
+ * @returns Promise with the open input and output endpoints
+ */
+export default async function UsbDriver({
+    vendorId,
+    productId,
+    device_interface_number,
+    endpoint_in_number,
+    endpoint_out_number
+} : {
+    vendorId: number;
+    productId: number;
+    device_interface_number: number;
+    endpoint_in_number: number;
+    endpoint_out_number: number;
+}): Promise<{
+    endpoint_in: InEndpoint;
+    endpoint_out: OutEndpoint;
+}>{
+    
+    const device = findByIds(vendorId, productId)
+    if (!device){
+        throw Error("Failed to find device")
+    }
+
+    device.open(true)
+
+    const error = await new Promise((cb: (error: LibUSBException | undefined) => void) => device.reset(cb))
+    if (error){
+        throw error
+    }
+
+    if (device.__isKernelDriverActive(device_interface_number)){
+        device.__detachKernelDriver(device_interface_number)
+    }
+
+    const intf = device.interface(device_interface_number)
+    const inEndpoint = intf.endpoint(endpoint_in_number)
+    const outEndpoint = intf.endpoint(endpoint_out_number)
+
+    if (!inEndpoint || !outEndpoint){
+        throw Error("Failed to setup endpoints")
+    }else if (inEndpoint !instanceof InEndpoint){
+        throw Error("Failed to setup input endpoint")
+    }else if (outEndpoint !instanceof OutEndpoint){
+        throw Error("Failed to setup output endpoint")
+    }
+
+    const endpoint_in = inEndpoint as InEndpoint
+    const endpoint_out = outEndpoint as OutEndpoint
+
+    return {
+        endpoint_in,
+        endpoint_out
+    }
+
+}
+
+export abstract class AbstractUsbDriver extends AbstractDriver implements IUsbDriver{
 
     // Properties from IUsbDriver
-    abstract vendorId: string;
-    abstract productId: string;
     abstract bpp: BitsPerPixel;
     abstract base_address: number;
     abstract width: number;
@@ -27,6 +98,17 @@ export default abstract class AbstractUsbDriver extends AbstractDriver implement
 
     // Usb driver properties
     tag_num: number = 0;
+    endpoint_in: InEndpoint
+    endpoint_out: OutEndpoint
+
+    constructor(
+        endpoint_in: InEndpoint,
+        endpoint_out: OutEndpoint
+    ){
+        super()
+        this.endpoint_in = endpoint_in
+        this.endpoint_out = endpoint_out
+    }
 
     /**
      * Each request should have an identifier.
--- src/interfaces/IUsbDriver.tsx
+++ src/interfaces/IUsbDriver.tsx
@@ -7,15 +7,6 @@ import IDriver from "./IDriver";
  */
 export default interface IUsbDriver extends IDriver {
 
-    // The vendor and product IDs are what identify a specific
-    // USB device.
-    // You can find these by using the `lsusb` program.
-    // Expected output of that program is something like:
-    // Bus 001 Device 001: ID 1d6b:0002 Linux Foundation 2.0 root hub
-    // Where `1d6b` is the vendor ID and `0002` is the product ID.
-    vendorId: string;
-    productId: string;
-
     // The base memory address after which register commands can be run.
     // ie. Register commands run at base_address + VALUE.
     base_address: number;
