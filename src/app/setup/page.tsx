14e7878bc1b0b138825a6d862220eea0116ffca2
mcarr823 <136939846+mcarr823@users.noreply.github.com>
2024-06-25 17:58:14 +1000

    Updated setup page appearance, added heading and description, made responsive

--- src/app/setup/page.tsx
+++ src/app/setup/page.tsx
@@ -5,6 +5,7 @@ import SubmitButton from '../components/SubmitButton';
 import Toast from '../components/Toast';
 import ToastViewModel from '@/viewmodels/ToastViewModel';
 import { ChangeEventHandler } from 'react';
+import HorizontalLine from '../components/HorizontalLine';
 
 /**
  * Daemon setup screen.
@@ -48,63 +49,87 @@ export default function SetupPage(){
     }
 
     return (
-        <form>
-            <div className="mb-3">
-                <label htmlFor="driver" className="form-label">Connection Type</label>
-                <SimpleSelect
-                    id="driver"
-                    values={model.drivers}
-                    value={model.driver}
-                    setValue={model.setDriver}
-                    />
+        <div className="row justify-content-center">
+            <div className="col-12 col-sm-10 col-lg-9 col-xl-6">
+                <div className="card">
+                    <h2 className="card-header">
+                        Daemon Setup
+                    </h2>
+
+                    <div className="card-body">
+                        <p className="text-muted">
+                            These options apply when the program is setup to run in the background and receive API calls.
+                        </p>
+                        <p className="text-muted">
+                            It is not required for ordinary testing.
+                        </p>
+                        <HorizontalLine/>
+                        <form className="mt-3">
+                            <div className="mt-3 mb-3">
+                                <label htmlFor="driver" className="form-label">Connection Type</label>
+                                <SimpleSelect
+                                    id="driver"
+                                    values={model.drivers}
+                                    value={model.driver}
+                                    setValue={model.setDriver}
+                                    />
+                            </div>
+                            <div className="mb-3">
+                                <label htmlFor="panel" className="form-label">Panel</label>
+                                <SimpleSelect
+                                    id="panel"
+                                    values={model.panels}
+                                    value={model.panel}
+                                    setValue={model.setPanel}
+                                    />
+                            </div>
+                            <div className="mb-3 form-check">
+                                <label htmlFor="remote" className="form-check-label">Remote</label>
+                                <input
+                                    id="remote"
+                                    type="checkbox"
+                                    className="form-check-input"
+                                    checked={model.remote}
+                                    onChange={setRemote}
+                                    />
+                            </div>
+                            <div className="mb-3" style={hostAndPortStyles}>
+                                <label htmlFor="host" className="form-label">Host</label>
+                                <input
+                                    id="host"
+                                    type="text"
+                                    className="form-control"
+                                    defaultValue={model.host}
+                                    onChange={setHost}
+                                    />
+                            </div>
+                            <div className="mb-3" style={hostAndPortStyles}>
+                                <label htmlFor="port" className="form-label">Port</label>
+                                <input
+                                    id="port"
+                                    type="text"
+                                    className="form-control"
+                                    defaultValue={model.port}
+                                    onChange={setPort}
+                                    />
+                            </div>
+                            
+                        </form>
+
+                    </div>
+
+                    <div className="card-footer text-end">
+                        <SubmitButton
+                            classes="btn btn-success"
+                            text="Save"
+                            enabled={!model.saving}
+                            onClick={save}
+                            />
+                    </div>
+                </div>
             </div>
-            <div className="mb-3">
-                <label htmlFor="panel" className="form-label">Panel</label>
-                <SimpleSelect
-                    id="panel"
-                    values={model.panels}
-                    value={model.panel}
-                    setValue={model.setPanel}
-                    />
-            </div>
-            <div className="mb-3 form-check">
-                <label htmlFor="remote" className="form-check-label">Remote</label>
-                <input
-                    id="remote"
-                    type="checkbox"
-                    className="form-check-input"
-                    checked={model.remote}
-                    onChange={setRemote}
-                    />
-            </div>
-            <div className="mb-3" style={hostAndPortStyles}>
-                <label htmlFor="host" className="form-label">Host</label>
-                <input
-                    id="host"
-                    type="text"
-                    className="form-control"
-                    defaultValue={model.host}
-                    onChange={setHost}
-                    />
-            </div>
-            <div className="mb-3" style={hostAndPortStyles}>
-                <label htmlFor="port" className="form-label">Port</label>
-                <input
-                    id="port"
-                    type="text"
-                    className="form-control"
-                    defaultValue={model.port}
-                    onChange={setPort}
-                    />
-            </div>
-            <SubmitButton
-                classes="btn btn-success mt-3"
-                text="Save"
-                enabled={!model.saving}
-                onClick={save}
-                />
             <Toast model={toast}/>
-        </form>
+        </div>
     )
 }
 
